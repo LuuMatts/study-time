863c3942b26617883a1c6f434c43dff902afd263
Gian-Luca Zutz <luca@matter.de>
2024-02-08 06:15:08 +0100

    updated-url

--- obs_scene.py
+++ obs_scene.py
@@ -4,7 +4,7 @@ import threading
 import ssl
 
 # Replace with your Replit WebSocket server URL
-url = "ws://studysound4-env.eba-e3jmgwhv.eu-north-1.elasticbeanstalk.com/"
+url = "wss://app.studywithluca.com"
 
 # Lists of scene names for each state
 study_scenes = [
